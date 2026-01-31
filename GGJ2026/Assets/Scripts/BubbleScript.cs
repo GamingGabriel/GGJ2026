7961138395eefa209b7ae00ffbfbd7e1c31ec34b
GamingGabriel <gabrieljrosss@gmail.com>
2026-01-31 15:56:45 -0500

    Get Key Pressed
    Input for Keys pressed.

---
+++ GGJ2026/Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BubbleScript : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro dialogueBubble;
+
+    [SerializeField]
+    private Text dialogueText;
+
+    [SerializeField]
+    private string dialogueWords;
+
+    [SerializeField]
+    private char BUTTONTOPRESS;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
