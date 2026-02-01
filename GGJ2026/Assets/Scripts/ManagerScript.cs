7df6fabdf9b5fff4620e87fac82f11e4871168c6
GamingGabriel <gabrieljrosss@gmail.com>
2026-01-31 21:37:21 -0500

    Bubbles Spawn
    Bubbles now spawn at a fixed rate, in a random position

--- GGJ2026/Assets/Scripts/ManagerScript.cs
+++ GGJ2026/Assets/Scripts/ManagerScript.cs
@@ -4,40 +4,58 @@ using UnityEngine.InputSystem;
 
 public class ManagerScript : MonoBehaviour
 {
-    BubbleScript[] bubbles; 
+    [SerializeField]
+    private float spawnRate;
+    [SerializeField]
+    BubbleScript[] bubbleScripts; 
+    [SerializeField]
     GameObject[] spawnPoints;
+    [SerializeField]
+    GameObject[] bubbles;
+    [SerializeField]
+    private float lastSpawn;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spawnPoints = GameObject.FindGameObjectsWithTag("Spawnpoint");
+        lastSpawn = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(Time.time);
         string i = Input.inputString; 
-        bubbles = FindObjectsByType<BubbleScript>(FindObjectsSortMode.None);
+        bubbleScripts = FindObjectsByType<BubbleScript>(FindObjectsSortMode.None);
         //print(i);
-        if (bubbles.Length > 0)
+        if (bubbleScripts.Length > 0)
         {
-            //bool popped = false;
+            bool popped = false;
             //print(bubbles.Length);
-            foreach(BubbleScript b in bubbles)
+            foreach(BubbleScript b in bubbleScripts)
             {
                 print(b.getButtonToPress());
                 //while (!popped)
                 //{
                     if (i == b.getButtonToPress().ToString())
                     {
+                        if (!popped)
                         b.Pop();
-                        //popped = true;
+                        popped = true;
                     }
                // }
                 
             }
         }
-        
-        
+        if (Time.time - lastSpawn > spawnRate)
+        {
+            GameObject chosenSpawn = spawnPoints[(int)Random.Range(0, spawnPoints.Length -1)];
+            GameObject chosenBubble = bubbles[(int)Random.Range(0, bubbles.Length -1)];
+            print(chosenSpawn);
+            Instantiate(chosenBubble, chosenSpawn.transform);
+            lastSpawn = Time.time;
+        } 
     }
 
 }
